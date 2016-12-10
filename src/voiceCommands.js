a5c327b6209d16a12dff43a0035c0782bce528a6
bryson-reynolds <bryson@brysonreynolds.com>
2016-12-10 16:34:40 -0600

    adding voice commands and annyang

---
+++ src/voiceCommands.js
@@ -0,0 +1,37 @@
+const voiceCommands = {
+    'pull now': () => {
+        alert('pull now');
+    },
+    'legs out': () => {
+        alert('legs out');
+    },
+    'legs in': () => {
+        alert('legs in');
+    },
+    'relax': () => {
+        alert('relax');
+    },
+    'check arms': () => {
+        alert('check arms');
+    },
+    'pelvis forward': () => {
+        alert('pelvis forward');
+    },
+    'arch': () => {
+        alert('arch');
+    },
+    'circle of awareness': () => {
+        alert('circle of awareness');
+    },
+    'release pilot chute': () => {
+        alert('release pilot chute');
+    },
+    'ok': () => {
+        alert('ok');
+    },
+    'toe taps': () => {
+        alert('toe taps');
+    }
+}
+
+export default voiceCommands;
